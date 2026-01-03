bf7fa1e65b5c15e838e6320975d4dcdc337c6fe6
Deghost214 <144705704+Deghost214@users.noreply.github.com>
2026-01-03 14:28:32 +0530

    Delete JAVA2024 directory

--- JAVA2024/Input.java
+++
@@ -1,8 +0,0 @@
-public class Input {
-    public static void main(String[] args) {
-
-            int num1 = Integer.parseInt(args[0]);
-            int num2 = Integer.parseInt(args[1]);
-        System.out.println("The sum = "+ (num1+num2));
-    }
-}
